f624f3dbfa4c5dee90a881ac32393f2c803fe494
SathyaveluBalaji <balaji.s29200@gmail.com>
2024-03-08 16:37:03 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'weldingwebsite';
+
+  slideIndex = 1;
+
+  public plusSlides(n: number) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  public currentSlide(n: number) {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  showSlides(n: number) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
+    //let dots = document.getElementsByClassName("dot") as HTMLCollectionOf<HTMLElement>;
+
+    if (n > slides.length) {
+      this.slideIndex = 1;
+    }
+    if (n < 1) {
+      this.slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    // for (i = 0; i < dots.length; i++) {
+    //   dots[i].className = dots[i].className.replace(" active", "");
+    // }
+
+    slides[this.slideIndex - 1].style.display = "block";
+    //dots[this.slideIndex - 1].className += " active";
+  }
+}
+
---
+++ src/app/components/dashboard/dasboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-studentdetails',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class dashboard implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  }
